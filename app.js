49b7620a50aadc236eebe6136002269200edc291
Turbo Xav <xavier.tagliarino@gmail.com>
2022-01-13 17:10:49 +0100

    test forever start

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const https = require('https')
-const path = require('path')
-const fs = require('fs')
+const express = require("express");
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
 
-const app = express()
+const app = express();
 
-app.use('/', (req, res, next) => {
-  res.send('Hello from SSL server')
-})
+app.use("/", (req, res, next) => {
+  res.send("Hello from SSL server");
+});
 
 const sslServer = https.createServer(
   {
     // Auto signé
-    key: fs.readFileSync(path.join(__dirname, 'cert2', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert2', 'cert.pem')),
+    key: fs.readFileSync(path.join(__dirname, "cert2", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert2", "cert.pem")),
     // ou certificat zero ssl pour meteo-back.projets.web.fr
     //key: fs.readFileSync(path.join(__dirname, 'cert2/meteo-back.projets-web.fr', 'private.key')),
     //cert: fs.readFileSync(path.join(__dirname, 'cert2/meteo-back.projets-web.fr', 'certificate.crt')),
   },
   app
-)
+);
 
-sslServer.listen(3443, () => console.log('Secure server 🚀🔑 on port 3443'))
+sslServer.listen(3443, () => console.log("Secure server 🚀🔑 on port 3443"));
